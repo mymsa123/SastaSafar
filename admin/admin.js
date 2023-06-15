e39ae3aebaba0f6207ec5a325404c05ca3938d5f
Priyap1038 <124154881+Priyap1038@users.noreply.github.com>
2023-06-15 21:47:15 +0530

    done with css and js part for admin

---
+++ admin/admin.js
@@ -0,0 +1,13 @@
+// Accesing All The elements From Html
+let baseurl=
+let TotalProductCount=document.getElementById("totalProducts")
+
+
+
+
+// Getting Number of Products
+fetch(`${baseurl}/tours`)
+    .then(res=>res.json())
+    .then(data=>{
+       TotalProductCount.innerText=data.length
+})
