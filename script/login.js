1e1ce409d167da310278bd8d6ec713b2f918177d
unknown <achalgaddamwar.ag@gmail.com>
2023-06-17 18:44:56 +0530

    Updated by Achal

--- script/login.js
+++ script/login.js
@@ -43,22 +43,28 @@ signinForm.addEventListener("submit", (event) => {
 
     const storedUser = JSON.parse(localStorage.getItem(username));
 
-    if(storedUser){
-        if(password === storedUser.password){ 
-            localStorage.setItem("isLoggedIn", "true");
-            localStorage.setItem("currentUser", username);
-            window.location.href = "index.html";
-        }
-        else {
-            alert("Invalid password.");
-        }
+    if(username==="admin" && password==="admin"){
+        window.location.href = "admin/admin.html";
     }
     else{
-        alert("User does not exist. Please sign up.");
-        signinForm.reset();
-        signUpLink.click();
+        if(storedUser){
+            if(password === storedUser.password){ 
+                localStorage.setItem("isLoggedIn", "true");
+                localStorage.setItem("currentUser", username);
+                window.location.href = "index.html";
+            }
+            else {
+                alert("Invalid password.");
+            }
+        }
+        else{
+            alert("User does not exist. Please sign up.");
+            signinForm.reset();
+            signUpLink.click();
+        }
     }
-});
+})
+
 
 function togglePassword(passwordInputId){
     var passwordInput = document.getElementById(passwordInputId);
